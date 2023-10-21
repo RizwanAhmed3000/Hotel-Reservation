2448d5286aff89db94c8e404e2082193bbdd5b61
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-10-21 21:19:33 +0500

    react date range

--- frontend/src/Components/Search/Search.jsx
+++ frontend/src/Components/Search/Search.jsx
@@ -1,7 +1,21 @@
 import "./search.css";
 import { CalendarTodayOutlined, KingBedOutlined, PersonOutlineOutlined } from '@mui/icons-material'
+import { DateRange } from 'react-date-range';
+import { useState } from "react";
+import 'react-date-range/dist/styles.css'; // main css file
+import 'react-date-range/dist/theme/default.css'; // theme css file
 
 export default function Search() {
+
+    const [date, setDate] = useState([
+        {
+            startDate: new Date(),
+            endDate: null,
+            key: 'selection'
+        }
+    ]);
+    const [showDate, setShowDate] = useState(false)
+
     return (
         <div className="search">
             <div className="searchWrapper">
@@ -9,13 +23,22 @@ export default function Search() {
                     <KingBedOutlined />
                     <input type="text" placeholder="Search Hotels..." className="searchHotelsInput" />
                 </div>
-                <div className="searchItems">
+                <div className="searchItems reactDateRange" onClick={() => setShowDate(!showDate)}>
                     <CalendarTodayOutlined />
-                    <span className="checkInDateInput">date to date</span>
+                    <span className="checkInDateInput" >date to date</span>
+                    {
+                        showDate && <DateRange
+                            editableDateInputs={true}
+                            onChange={item => setDate([item.selection])}
+                            moveRangeOnFirstSelection={false}
+                            ranges={date}
+                            className="dateRange"
+                        />
+                    }
                 </div>
                 <div className="searchItems">
                     <PersonOutlineOutlined />
-                    <span className="personSearch">2 adult 2 childern 1 room</span>
+                    <span className="personSearch optionsList">2 adult 2 childern 1 room</span>
                 </div>
                 <button className="searchBtn">Search</button>
             </div>
