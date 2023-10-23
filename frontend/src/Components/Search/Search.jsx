cccbcb408ab8134b3ac7439e5454df651fbbea64
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-10-23 12:13:59 +0500

    Search bar Ui

--- frontend/src/Components/Search/Search.jsx
+++ frontend/src/Components/Search/Search.jsx
@@ -16,6 +16,21 @@ export default function Search() {
         }
     ]);
     const [showDate, setShowDate] = useState(false)
+    const [showOpitons, setShowOptions] = useState(false)
+    const [opitons, setOptions] = useState({
+        adults: 1,
+        childern: 0,
+        room: 1,
+    })
+
+    function optionHandler(name, opertation) {
+        setOptions((prev) => {
+            return {
+                ...prev,
+                [name]: opertation === 'inc' ? opitons[name] + 1 : opitons[name] - 1
+            }
+        })
+    }
 
     return (
         <div className="search">
@@ -26,7 +41,7 @@ export default function Search() {
                 </div>
                 <div className="searchItems reactDateRange" onClick={() => setShowDate(!showDate)}>
                     <CalendarTodayOutlined />
-                    <span className="checkInDateInput" >{`${format(date[0]?.startDate, "dd/mm/yyyy")} to ${format(date[0]?.endDate, "dd/mm/yyyy")}`}</span>
+                    <span className="checkInDateInput" >{`${format(date[0]?.startDate, "dd/MM/yyyy")} to ${format(date[0]?.endDate, "dd/MM/yyyy")}`}</span>
                     {
                         showDate && <DateRange
                             editableDateInputs={true}
@@ -37,12 +52,54 @@ export default function Search() {
                         />
                     }
                 </div>
-                <div className="searchItems">
+                <div className="searchItems optionsList" >
                     <PersonOutlineOutlined />
-                    <span className="personSearch optionsList">2 adult 2 childern 1 room</span>
+                    <span className="personSearch " onClick={() => setShowOptions(!showOpitons)}>{`${opitons.adults} adult ${opitons.childern} childerns ${opitons.room} rooms`}</span>
+                    {
+                        showOpitons && <Options optionHandler={optionHandler} opitons={opitons} />
+                    }
                 </div>
                 <button className="searchBtn">Search</button>
             </div>
         </div>
+    )
+}
+
+function Options({ optionHandler, opitons }) {
+    return (
+        <>
+            <div className="optionsBox">
+                <div className="option">
+                    <span className="optionText">
+                        Adult
+                    </span>
+                    <div className="optionCounter">
+                        <button disabled={opitons.adults <= 1} className="optionCounterMinus" onClick={() => optionHandler("adults", "dec")}>-</button>
+                        <span className="optionCounterNumber">{opitons.adults}</span>
+                        <button className="optionCounterPlus" onClick={() => optionHandler("adults", "inc")}>+</button>
+                    </div>
+                </div>
+                <div className="option">
+                    <span className="optionText">
+                        Children
+                    </span>
+                    <div className="optionCounter">
+                        <button disabled={opitons.childern <= 0} className="optionCounterMinus" onClick={() => optionHandler("childern", "dec")}>-</button>
+                        <span className="optionCounterNumber">{opitons.childern}</span>
+                        <button className="optionCounterPlus" onClick={() => optionHandler("childern", "inc")}>+</button>
+                    </div>
+                </div>
+                <div className="option">
+                    <span className="optionText">
+                        Room
+                    </span>
+                    <div className="optionCounter">
+                        <button disabled={opitons.room <= 1} className="optionCounterMinus" onClick={() => optionHandler("room", "dec")}>-</button>
+                        <span className="optionCounterNumber">{opitons.room}</span>
+                        <button className="optionCounterPlus" onClick={() => optionHandler("room", "inc")}>+</button>
+                    </div>
+                </div>
+            </div>
+        </>
     )
 }
