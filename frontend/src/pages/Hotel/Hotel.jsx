5fd1d03d1d0e947636f3bce483a3001ab977be40
RizwanAhmed3000 <rizz112khan@gmail.com>
2023-11-06 11:51:08 +0500

    Hotel page Ui done

--- frontend/src/pages/Hotel/Hotel.jsx
+++ frontend/src/pages/Hotel/Hotel.jsx
@@ -1,6 +1,8 @@
 import './hotel.css';
 import Navbar from '../../Components/Navbar/Navbar'
 import Header from '../../Components/Header/Header';
+import MailList from '../../Components/mailList/MailList';
+import Footer from '../../Components/footer/Footer';
 import { LocationOn } from '@mui/icons-material';
 
 export default function Hotel() {
@@ -32,9 +34,10 @@ export default function Hotel() {
             <Header type={"hotelList"} />
             <div className="hotelContainer">
                 <div className="hotelWrapper">
+                    <button className="bookNow lsBtn">Reserve or Book now</button>
                     <h1 className='hotelTitle'>Hotel Name</h1>
                     <div className="hotelAddress">
-                        <LocationOn />
+                        <LocationOn fontSize='15px' />
                         <span>this is the address</span>
                     </div>
                     <div className="hotelDistance">
@@ -52,8 +55,26 @@ export default function Hotel() {
                             ))
                         }
                     </div>
+                    <div className="hotelDetails">
+                        <div className="hotelDetailsTexts">
+                            <h1 className="hotelDetailTitle">Stay in the heart of Pakistan</h1>
+                            <p className="hdDesc">
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. A autem, necessitatibus quia corrupti officia ullam et omnis id animi adipisci natus culpa accusantium rem dicta at labore provident consectetur similique repudiandae unde maxime. Voluptate vero quidem laudantium natus placeat odit incidunt error ex? Expedita iste magnam reprehenderit voluptate, a explicabo quidem dolorem quibusdam doloremque ipsum voluptatum itaque adipisci, numquam voluptates id? Maiores cum quo vero autem explicabo dicta facere qui culpa atque rerum illum, nihil corporis iure fugit? Corrupti dolorum alias dicta cum, dolore deleniti tempora minima. Ea quae enim repellendus id nostrum, nam, quas eum iste minima perferendis culpa.
+                            </p>
+                        </div>
+                        <div className="hotelDetailsPrice">
+                            <h1>Perfect for 9-night stay</h1>
+                            <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam reiciendis atque dolores rem ex cupiditate deleniti.</span>
+                            <h2>
+                                <b>PKR 144,000</b> for 9-nights
+                            </h2>
+                            <button className='lsBtn'>Reserve or Book now</button>
+                        </div>
+                    </div>
                 </div>
             </div>
+            <MailList />
+            <Footer />
         </div>
     )
 }
